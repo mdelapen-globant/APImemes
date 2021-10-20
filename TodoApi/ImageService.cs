e5ef09df96c759d0cb3ae3fd3413784ec6464b4d
Macario De La Pena Valencia <macario.delapena@globant.com>
2021-10-20 17:14:26 -0600

    added business logic to ImageService Class

--- TodoApi/ImageService.cs
+++ TodoApi/ImageService.cs
@@ -2,10 +2,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace TodoApi
 {
     public class ImageService
     {
+        //load the image file
+        private Bitmap RenderImage(string imagePath)
+        {
+            Bitmap image = (Bitmap)Image.FromFile(imagePath);
+            return image;
+        }
+
+        //Concatenate text and image with x and y coordinates
+        public void AddTextToImage(Bitmap image, string text, int xCoordinate, int yCoordinate)
+        {
+            Point point = new Point(xCoordinate, yCoordinate); //combine coordinates
+
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                using (Font arialFont = new Font("Arial", 10))
+                {
+                    graphics.DrawString(text, arialFont, Brushes.Black, point); //add text to given image
+                }
+            }
+        }
+
+        //save image in wwwroot
+        public string SaveImageInRoot(Bitmap image, string imageName)
+        {
+            string path = "~/wwroot/img/created_memes/" + imageName;
+            image.Save(path);
+            return path;
+        }
+
+
+        public string CreateMeme(string imagePath, string[] textColection, int[,] coordinatesCollection)
+        {
+            Bitmap meme = RenderImage(imagePath);
+
+            for (int i = 0; i < textColection.Length; i++) //add all the text inputs to single image
+            {
+                AddTextToImage(meme, textColection[i], coordinatesCollection[i, 0], coordinatesCollection[i, 1]);
+            }
+
+            return SaveImageInRoot(meme,"meme1.jpg");
+        }
     }
 }
--- TodoApi/MemeService.cs
+++ TodoApi/MemeService.cs
@@ -2,11 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TodoApi.Models;
 
 namespace TodoApi
 {
     public class MemeService
     {
 
+        //receive meme image with path and text
+        public void CreateMeme(MemeImage memeImage, List<TextCoordinates> coordinates)
+        {
+            //call image builder and concatenate text with image
+        }
     }
 }
