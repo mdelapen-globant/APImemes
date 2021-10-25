bc99d044eddaa4777bde4a62821b8a3b8d821de4
Macario De La Pena Valencia <macario.delapena@globant.com>
2021-10-25 17:17:22 -0600

    fixed text coordinates for each meme

--- TodoApi/Controllers/MemeController.cs
+++ TodoApi/Controllers/MemeController.cs
@@ -22,81 +22,13 @@ namespace TodoApi.Controllers
         }
 
         // GET: api/meme
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<MemeImage>>> GetMemeImages()
-        //{
-        //    return await _context.MemeImages.ToListAsync();
-        //}
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<MemeImage>>> GetMemeImages()
+        {
+            return await _context.MemeImages.ToListAsync();
+        }
 
-        //////GET: api/meme/5
-        //[HttpGet("{id}")]
-        //public async Task<ActionResult<MemeImage>> GetMemeImage(long id)
-        //{
-        //    var memeImage = await _context.MemeImages.FindAsync(id);
 
-        //    if (memeImage == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return memeImage;
-        //}
-
-        //// PUT: api/meme/5
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutMemeImage(int id, MemeImage memeImage)
-        //{
-        //    if (id != memeImage.Id)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(memeImage).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!MemeImageExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-        //// POST: api/meme
-        ////[HttpPost]
-        ////public async Task<ActionResult<MemeImage>> PostMemeImage(MemeImage memeImage)
-        ////{
-        ////    _context.MemeImages.Add(memeImage);
-        ////    await _context.SaveChangesAsync();
-
-        ////    return CreatedAtAction(nameof(GetMemeImage), new { id = memeImage.Id }, memeImage);
-        ////}
-
-        //// DELETE: api/meme/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteMemeImage(int id)
-        //{
-        //    var items = await _context.MemeImages.FindAsync(id);
-        //    if (items == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.MemeImages.Remove(items);
-        //    await _context.SaveChangesAsync();
-
-        //    return NoContent();
-        //}
 
         [HttpGet("{memeId}")]
         //[Route("create")]
